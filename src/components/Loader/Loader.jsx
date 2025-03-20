f276e8efdb05b02d0eb788771b5361e1eb93d5d5
Larysa Tolkachova <seagull2610@gmail.com>
2025-03-20 14:41:48 +0100

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,3 @@
 export default function Loader() {
   return <b>Loading data, please wait...</b>;
 }
-
-// =============== =
-// import { InfinitySpin } from 'react-loader-spinner';
-// import s from './Loader.module.css';
-
-// const Loader = () => {
-//   return (
-//     <div className={s.loader_wrapper}>
-//       <InfinitySpin visible={true} width="200" color="#4800fd" ariaLabel="infinity-spin-loading" />
-//     </div>
-//   );
-// };
-
-// export default Loader;
