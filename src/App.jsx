3e404ab3efd567894b4ab4839e2768680e200ebd
Larysa <seagull2610@gmail.com>
2025-03-20 11:51:28 +0100

    load more

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { fetchArt } from './api';
 import SearchBar from './components/SearchBar/SearchBar';
@@ -11,27 +11,68 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  //запит
-  const handleSearch = async query => {
-    try {
-      setError(false);
-      setLoading(true);
-      setImages([]);
-      const request = await fetchArt(query);
-      setImages(request);
-    } catch {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+
+  //запит до Load more
+  // const handleSearch = async query => {
+  //   try {
+  //     setError(false);
+  //     setLoading(true);
+  //     setImages([]);
+  //     const request = await fetchArt(query);
+  //     setImages(request);
+  //   } catch {
+  //     setError(true);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  const handleSearch = newImages => {
+    setSearch(newImages);
+    setPage(1);
+    setImages([]);
   };
+
+  useEffect(() => {
+    if (search === '') {
+      return;
+    }
+
+    async function getImages() {
+      try {
+        setError(false);
+        setLoading(true);
+        const request = await fetchArt(page, search);
+        setImages(prevImages => {
+          return [...prevImages, ...request];
+        });
+      } catch {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getImages();
+    console.log(page);
+  }, [page, search]);
 
   return (
     <>
       <SearchBar onSearch={handleSearch} />
-      {loading && <Loader />}
       {error && <ErrorMessage />}
       {images.length > 0 && <ImageGallery items={images} />}
+      {loading && <Loader />}
+      {images.length > 0 && (
+        <button
+          onClick={() => {
+            setPage(page + 1);
+          }}
+        >
+          Load more
+        </button>
+      )}
     </>
   );
 }
--- src/api.js
+++ src/api.js
@@ -3,10 +3,15 @@ import axios from 'axios';
 const BASE_URL = 'https://api.unsplash.com/search/photos';
 const API_KEY = 'm65ndF3IYl30TtHqwAa_2zxVBk9EVDDctDHqfKVuPhQ';
 
-export const fetchArt = async query => {
-  const response = await axios.get(`${BASE_URL}?client_id=${API_KEY}&query=${query}&per_page=3`);
+export const fetchArt = async (query, page) => {
+  const response = await axios.get(`${BASE_URL}`, {
+    params: {
+      client_id: API_KEY,
+      query: query,
+      page: page,
+      per_page: 3,
+    },
+  });
 
   return response.data.results;
 };
-// const response = await axios.get(`${BASE_URL}?query=${query}&page=${page}&per_page=20&client_id=${API_KEY}`);
-// query, page;
